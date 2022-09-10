594d78564565ce387fa5ef98df46e46001bfa109
Preetam <preetamchhipa759@gmail.com>
2022-09-10 10:43:48 +0530

    solved

--- sort.py
+++ sort.py
@@ -1,12 +1,12 @@
 # A function that returns the 'year' value:
 def myFunc(e):
-  return e'year'
+  return e['year']
 
-cars = 
+cars = [
   {'car': 'Ford', 'year': 2005},
   {'car': 'Mitsubishi', 'year': 2000},
   {'car': 'BMW', 'year': 2019},
   {'car': 'VW', 'year': 2011}
-
+  ]
 
 cars.sort(key=myFunc)
